e8fa4485dc466faf475e52748cd55d2bcff8dae5
star <153933116+KarthikDulla@users.noreply.github.com>
2026-02-09 10:07:46 +0530

    Add files via upload

---
+++ Rotation.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+image = cv2.imread("sample.jpg")
+
+height, width = image.shape[:2]
+center = (width // 2, height // 2)
+
+angle = 45
+scale = 1.0
+
+rotate_matrix = cv2.getRotationMatrix2D(center, angle, scale)
+rotate_image = cv2.warpAffine(image, rotate_matrix, (width, height))
+
+cv2.imshow("Rotate Image", rotate_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
